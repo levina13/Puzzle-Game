e2ed01b4a795bfd377e01f870243c52eb4bb4da2
levina13 <87984577+levina13@users.noreply.github.com>
2023-11-13 11:32:34 +0700

    implement boundaries for pieces

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -1,14 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class DragAndDrop : MonoBehaviour
 {
     public GameObject SelectedPiece;
+    int OIL = 1;
     // Start is called before the first frame update
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Confined;
     }
 
     // Update is called once per frame
@@ -22,21 +24,46 @@ public class DragAndDrop : MonoBehaviour
                 if (!hit.transform.GetComponent<PiecesScript>().InRightPosition)
                 {
                     SelectedPiece = hit.transform.gameObject;
+                    SelectedPiece.GetComponent<PiecesScript>().BeforePosition = SelectedPiece.transform.position;
                     SelectedPiece.GetComponent<PiecesScript>().Selected = true;
+                    SelectedPiece.GetComponent<SortingGroup>().sortingOrder = OIL;
+                    OIL++;
+                    Debug.Log("down");
                 }
             }
 
-        }
-        if (Input.GetMouseButtonUp(0))
-        {
-            SelectedPiece.GetComponent<PiecesScript>().Selected = false;
-            SelectedPiece = null;
         }
         if (SelectedPiece != null)
         {
             Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             SelectedPiece.transform.position = new Vector3(MousePoint.x, MousePoint.y, 0);
         }
+        if (Input.GetMouseButtonUp(0))
+        {
+            if (SelectedPiece != null)
+            {
+                // if di luar jangkauan
+                bool inside = SelectedPiece.GetComponent<PiecesScript>().Inside;
+                if (!inside)
+                {
+                    SelectedPiece.transform.position = SelectedPiece.GetComponent<PiecesScript>().BeforePosition;
+                }
+                SelectedPiece.GetComponent<PiecesScript>().Selected = false;
+                SelectedPiece = null;
+            }
+        }
+
 
     }
+    // void OnApplicationFocus(bool onFocus)
+    // {
+    //     if (onFocus)
+    //     {
+    //         Cursor.lockState = CursorLockMode.Locked;
+    //     }
+    //     else
+    //     {
+    //         Debug.Log("lost focus");
+    //     }
+    // }
 }
--- Assets/Scripts/PiecesScript.cs
+++ Assets/Scripts/PiecesScript.cs
@@ -1,19 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class PiecesScript : MonoBehaviour
 {
     public Vector3 RightPosition;
+    public Vector3 BeforePosition;
     public bool InRightPosition;
     public bool Selected;
+    public bool Inside;
     public float distance;
     // Start is called before the first frame update
     void Start()
     {
 
         RightPosition = transform.position;
-        transform.position = new Vector3(Random.Range(-1.49f, 0.399f), Random.Range(-0.913f, -1.041f));
+        transform.position = new Vector3(Random.Range(-1.49f, 0.399f), Random.Range(-0.913f, -0.941f));
     }
 
     // Update is called once per frame
@@ -24,9 +28,25 @@ public class PiecesScript : MonoBehaviour
         {
             if (!Selected)
             {
-                transform.position = RightPosition;
-                InRightPosition = true;
+                if (InRightPosition == false)
+                {
+                    transform.position = RightPosition;
+                    InRightPosition = true;
+                    GetComponent<SortingGroup>().sortingOrder = 0;
+                }
             }
         }
+    }
+
+    void OnTriggerStay2D(Collider2D collider)
+    {
+        Inside = true;
+        Debug.Log("di dalam");
+    }
+
+    void OnTriggerExit2D(Collider2D collider)
+    {
+        Debug.Log("no longer colided");
+        Inside = false;
     }
 }
