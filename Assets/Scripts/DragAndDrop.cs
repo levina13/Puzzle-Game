b942d400f5e6969f4503241f22b010267c820580
levina13 <87984577+levina13@users.noreply.github.com>
2023-11-15 21:40:25 +0700

    buat manage Scene and level

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class DragAndDrop : MonoBehaviour
 {
@@ -11,14 +12,62 @@ public class DragAndDrop : MonoBehaviour
     int OIL = 1;
     public int TotalPiece;
     public int CountRightPiece;
+    public TMP_Text WaktuSisa;
+    public static float batasWaktu = 60f;
     // Start is called before the first frame update
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
+        if (StaticVar.level >= 1 && StaticVar.level <= 10)
+        {
+            TotalPiece = 24;
+            batasWaktu = (StaticVar.level > 5) ? 150f : 120f;
+        }
+        else if (StaticVar.level >= 11 && StaticVar.level <= 20)
+        {
+            TotalPiece = 48;
+            batasWaktu = (StaticVar.level > 15) ? 210f : 180f;
+        }
+        else
+        {
+            TotalPiece = 80;
+            batasWaktu = (StaticVar.level > 25) ? 300f : 240f;
+        }
     }
 
     // Update is called once per frame
     void Update()
+    {
+        if (batasWaktu > 0)
+        {
+            DisplayTime(batasWaktu);
+            PuzzleMove();
+            batasWaktu -= Time.deltaTime;
+        }
+        else
+        {
+            Lose();
+        }
+    }
+
+    public void Win()
+    {
+        manageScene.Success();
+    }
+
+    public void Lose()
+    {
+        manageScene.Lose();
+    }
+
+    public void DisplayTime(float timeToDisplay)
+    {
+        float minutes = Mathf.FloorToInt(timeToDisplay / 60);
+        float seconds = Mathf.FloorToInt(timeToDisplay % 60);
+        WaktuSisa.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+    }
+
+    public void PuzzleMove()
     {
         if (Input.GetMouseButtonDown(0))
         {
@@ -56,23 +105,5 @@ public class DragAndDrop : MonoBehaviour
             }
         }
 
-
     }
-
-    public void Win()
-    {
-        // change scene to win
-        manageScene.ChangeScene(0);
-    }
-    // void OnApplicationFocus(bool onFocus)
-    // {
-    //     if (onFocus)
-    //     {
-    //         Cursor.lockState = CursorLockMode.Locked;
-    //     }
-    //     else
-    //     {
-    //         Debug.Log("lost focus");
-    //     }
-    // }
 }
---
+++ Assets/Scripts/StaticVar.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StaticVar : MonoBehaviour
+{
+    public static int level = 0;
+    // Update is called once per frame
+
+}
