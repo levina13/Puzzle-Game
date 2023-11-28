a7c0b9490772c777bf03adc4a7aa2340d3d24a13
levina13 <87984577+levina13@users.noreply.github.com>
2023-11-28 15:21:01 +0700

    perbaiki +++

--- Assets/Scripts/DragAndDrop.cs
+++ Assets/Scripts/DragAndDrop.cs
@@ -25,22 +25,22 @@ public class DragAndDrop : MonoBehaviour
     void Start()
     {
         Cursor.lockState = CursorLockMode.Confined;
-        if (StaticVar.level >= 1 && StaticVar.level <= 10)
+        if (StaticVar.level >= 1 && StaticVar.level <= 5)
         {
-            TotalPiece = 24;
-            // batasWaktu = (StaticVar.level > 5) ? 150f : 120f;
+            TotalPiece = 8;
+            batasWaktu = 60f;
         }
-        else if (StaticVar.level >= 11 && StaticVar.level <= 20)
+        else if (StaticVar.level >= 6 && StaticVar.level <= 10)
         {
-            TotalPiece = 48;
-            // batasWaktu = (StaticVar.level > 15) ? 210f : 180f;
+            TotalPiece = 12;
+            batasWaktu = 180f;
         }
         else
         {
-            TotalPiece = 80;
-            // batasWaktu = (StaticVar.level > 25) ? 300f : 240f;
+            TotalPiece = 24;
+            batasWaktu = 300f;
         }
-        batasWaktu = 600f;
+        batasWaktu = 5f;
 
         progressBar.SetMaxProgress(TotalPiece);
 
